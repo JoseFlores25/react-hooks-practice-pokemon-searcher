09529f14db6586711003a10c5f816addbd3c85c7
Jose <flores.jose2185@gmail.com>
2022-03-11 23:27:40 -0500

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PokemonPage from "./PokemonPage";
+import PokemonCard from "./PokemonCard";
 
 function App() {
   return (
     <div className="App">
       <PokemonPage />
+      <PokemonCard />
     </div>
   );
 }
